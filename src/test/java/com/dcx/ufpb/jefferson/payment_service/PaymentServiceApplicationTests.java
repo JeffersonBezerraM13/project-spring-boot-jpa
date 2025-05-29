0302bd22c405bc22f8ab3de7ab4192d0d72a7ca5
Jefferson Bezerra <jeffebezerram13@gmail.com>
2025-05-29 10:29:41 -0300

    Project created

---
+++ src/test/java/com/dcx/ufpb/jefferson/payment_service/PaymentServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dcx.ufpb.jefferson.payment_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymentServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
